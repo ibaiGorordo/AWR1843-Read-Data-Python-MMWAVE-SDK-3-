ddc51f7ef516d304ae926b911590d30c56eeea09
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2019-10-22 16:25:10 +0900

    corrected buffer data removal

--- readData_AWR1843.py
+++ readData_AWR1843.py
@@ -141,8 +141,9 @@ def readAndParseData18xx(Dataport, configParameters):
         if startIdx:
             
             # Remove the data before the first start index
-            if startIdx[0] > 0:
+            if startIdx[0] > 0 and startIdx[0] < byteBufferLength:
                 byteBuffer[:byteBufferLength-startIdx[0]] = byteBuffer[startIdx[0]:byteBufferLength]
+                byteBuffer[byteBufferLength-startIdx[0]:] = np.zeros(len(byteBuffer[byteBufferLength-startIdx[0]:]),dtype = 'uint8')
                 byteBufferLength = byteBufferLength - startIdx[0]
                 
             # Check that there have no errors with the byte buffer length
@@ -231,6 +232,7 @@ def readAndParseData18xx(Dataport, configParameters):
             
                 
             byteBuffer[:byteBufferLength - shiftSize] = byteBuffer[shiftSize:byteBufferLength]
+            byteBuffer[byteBufferLength - shiftSize:] = np.zeros(len(byteBuffer[byteBufferLength - shiftSize:]),dtype = 'uint8')
             byteBufferLength = byteBufferLength - shiftSize
             
             # Check that there are no errors with the buffer length
@@ -252,13 +254,13 @@ def update():
     # Read and parse the received data
     dataOk, frameNumber, detObj = readAndParseData18xx(Dataport, configParameters)
     
-    if dataOk:
+    if dataOk and len(detObj["x"])>0:
         #print(detObj)
         x = -detObj["x"]
         y = detObj["y"]
         
-    s.setData(x,y)
-    QtGui.QApplication.processEvents()
+        s.setData(x,y)
+        QtGui.QApplication.processEvents()
     
     return dataOk
 
